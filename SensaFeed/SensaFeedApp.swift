52e84bc24f47d62b57fec9d3695a0cf720ac594a
Vasyl Holinwey <holineybest@gmail.com>
2026-02-06 16:11:59 +0200

    Initial Commit

---
+++ SensaFeed/SensaFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensaFeedApp.swift
+//  SensaFeed
+//
+//  Created by Vasyl Holiney on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SensaFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SensaFeedTests/SensaFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  SensaFeedTests.swift
+//  SensaFeedTests
+//
+//  Created by Vasyl Holiney on 06/02/2026.
+//
+
+import Testing
+@testable import SensaFeed
+
+struct SensaFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
